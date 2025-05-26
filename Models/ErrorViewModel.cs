ab74806eb3d254b3798d59e7161783e4c48a9716
Diego-tech977 <diegoalejandro.ramirezfrias.gen23@cbtis105.edu.mx>
2025-05-26 09:25:09 -0600

    olaaaa

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace proyecto_p3.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
---
+++ proyectop3/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace proyectop3.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
